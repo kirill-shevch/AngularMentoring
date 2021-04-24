497efc131baf4646c823a6c6f0ce3a1ae0806cfe
Kirill Shevchenko <kirillwowlol@yandex.ru>
2021-04-24 13:10:28 +0200

    Application split into the modules

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './components/product/product.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [ProductComponent, ProductListComponent],
+    exports: [ProductComponent, ProductListComponent]
+})
+export class ProductsModule { }
