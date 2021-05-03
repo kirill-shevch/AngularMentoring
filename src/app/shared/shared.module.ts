1a7cc5b1402c20b369cb8f4801e4ba0da259f324
Kirill Shevchenko <kirillwowlol@yandex.ru>
2021-05-03 22:40:43 +0200

    Add highlight directive for the cart list items

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HighlightDirective } from './Directives/highlight.directive';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  declarations: [
+    HighlightDirective
+  ],
+  exports: [
+    HighlightDirective
   ]
 })
 export class SharedModule { }
