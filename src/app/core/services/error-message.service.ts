07c1f5fb6e443a1a457279fa23ef7668c881ae92
Kirill Shevchenko <kirillwowlol@yandex.ru>
2021-07-14 21:37:31 +0200

    Add error message service

---
+++ src/app/core/services/error-message.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorMessageService {
+
+  get firstNameRequired(): string {
+    return "Please enter your first name.";
+  }
+
+  get firstNameMinLength(): string {
+    return "The first name must be longer than 3 characters.";
+  }
+
+  get firstNameCapitalLetter(): string {
+    return "The first name must start with a capital letter.";
+  }
+
+  get emailRequired(): string {
+    return "Please enter your email address.";
+  }
+
+  get emailInvalid(): string {
+    return "Please enter a valid email address.";
+  }
+
+  get phoneInvalid(): string {
+    return "Please enter a valid phone number.";
+  }
+}
